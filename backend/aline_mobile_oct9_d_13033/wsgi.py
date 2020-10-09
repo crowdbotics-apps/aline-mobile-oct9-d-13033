0b3d6a3d477c6ebcbf6e84df6744daeca12fbd63
Crowdbotics <team@crowdbotics.com>
2020-10-09 20:04:36 +0000

    Update models

--- backend/aline_mobile_oct9_d_13033/wsgi.py
+++ backend/aline_mobile_oct9_d_13033/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aline_mobile_oct9_d_13033.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aline_mobile_oct9_d_13033.settings")
 
 application = get_wsgi_application()
